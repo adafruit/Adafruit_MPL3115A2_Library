4e9eeb89d31627bfa9ec65532a5b03afc6eb9ca1
Travis CI <travis@travis-ci.org>
2018-01-24 22:12:37 +0000

    Deploy code docs to GitHub Pages Travis build: 8
    Commit: cc2ad8ef2f932e13c79a75f85c6bb8233a74d543

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -4,5 +4,5 @@ var searchData=
   ['adafruit_5fmpl3115a2_2ecpp',['Adafruit_MPL3115A2.cpp',['../_adafruit___m_p_l3115_a2_8cpp.html',1,'']]],
   ['adafruit_5fmpl3115a2_2eh',['Adafruit_MPL3115A2.h',['../_adafruit___m_p_l3115_a2_8h.html',1,'']]],
   ['adafruit_20mpl3115a2_20alitmeter',['Adafruit MPL3115A2 alitmeter',['../index.html',1,'']]],
-  ['adafruit_20mpl3115a2_20library_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fmpl3115a2_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fmpl3115a2_29',['Adafruit MPL3115A2 Library [![Build Status](https://travis-ci.org/adafruit/Adafruit_MPL3115A2.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_MPL3115A2)',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['adafruit_20mpl3115a2_20library_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fmpl3115a2_5flibrary_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fmpl3115a2_5flibrary_29',['Adafruit MPL3115A2 Library [![Build Status](https://travis-ci.org/adafruit/Adafruit_MPL3115A2_Library.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_MPL3115A2_Library)',['../md__r_e_a_d_m_e.html',1,'']]]
 ];
