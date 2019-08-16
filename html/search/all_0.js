1ec1a99fb212117e055353eb30eacdc01f70b2d7
Travis CI <travis@travis-ci.org>
2019-08-16 17:33:46 +0000

    Deploy code docs to GitHub Pages Travis build: 24
    Commit: 0820c19db3a14b0b02cfac4fb239eb7f41e462ad

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -3,6 +3,5 @@ var searchData=
   ['adafruit_5fmpl3115a2',['Adafruit_MPL3115A2',['../class_adafruit___m_p_l3115_a2.html',1,'Adafruit_MPL3115A2'],['../class_adafruit___m_p_l3115_a2.html#a186d5734c54fc8b3d75002edf6b5c226',1,'Adafruit_MPL3115A2::Adafruit_MPL3115A2()']]],
   ['adafruit_5fmpl3115a2_2ecpp',['Adafruit_MPL3115A2.cpp',['../_adafruit___m_p_l3115_a2_8cpp.html',1,'']]],
   ['adafruit_5fmpl3115a2_2eh',['Adafruit_MPL3115A2.h',['../_adafruit___m_p_l3115_a2_8h.html',1,'']]],
-  ['adafruit_20mpl3115a2_20alitmeter',['Adafruit MPL3115A2 alitmeter',['../index.html',1,'']]],
-  ['adafruit_20mpl3115a2_20library_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fmpl3115a2_5flibrary_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fmpl3115a2_5flibrary_29',['Adafruit MPL3115A2 Library [![Build Status](https://travis-ci.org/adafruit/Adafruit_MPL3115A2_Library.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_MPL3115A2_Library)',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['adafruit_20mpl3115a2_20alitmeter',['Adafruit MPL3115A2 alitmeter',['../index.html',1,'']]]
 ];
--- html/search/pages_0.js
+++ html/search/pages_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['adafruit_20mpl3115a2_20alitmeter',['Adafruit MPL3115A2 alitmeter',['../index.html',1,'']]],
-  ['adafruit_20mpl3115a2_20library_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fmpl3115a2_5flibrary_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fmpl3115a2_5flibrary_29',['Adafruit MPL3115A2 Library [![Build Status](https://travis-ci.org/adafruit/Adafruit_MPL3115A2_Library.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_MPL3115A2_Library)',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['adafruit_20mpl3115a2_20alitmeter',['Adafruit MPL3115A2 alitmeter',['../index.html',1,'']]]
 ];
